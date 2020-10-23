39d57604bfc46408cab7cd6c057a39dd60df0ca7
tacotown <mjenison333@gmail.com>
2020-10-22 21:43:45 -0500

    super

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+const host = process.env.HOST;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  const host = process.env.HOST;
   while(true) {
     console.log(`Suck it, ${host}!`);
     await sleep(5000);
