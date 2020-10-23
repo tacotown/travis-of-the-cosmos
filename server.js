277ac3a34926639fbe55ed48d2ba9658004d8d93
tacotown <mjenison333@gmail.com>
2020-10-22 20:53:02 -0500

    fixed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 async function main() {
   const host = process.env.HOST;
   while(true) {
-    console.log("Suck it, ${host}!");
+    console.log(`Suck it, ${host}!`);
     await sleep(5000);
   }
 }
